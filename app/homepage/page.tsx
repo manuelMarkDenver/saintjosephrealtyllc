57b408d1c2061d67f49956c65f1dba782237016b
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-02-22 22:12:53 +0800

    installed mui, tailwind and wip - navbar

---
+++ app/homepage/page.tsx
@@ -0,0 +1,13 @@
+// import { Inter } from '@next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Homepage() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Saint Joseph Real Estate LLC.
+      </h1>
+    </>
+  );
+}
