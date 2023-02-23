d768d9ad949f8b17e22b62987dc13025a90fac30
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-02-24 03:05:39 +0800

    regex to exclude mp4

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import Homepage from "./Homepage/page";
 const imagesDir = path.join(process.cwd(), "public/images/projects/project-1");
 const imageNames = fs.readdirSync(imagesDir);
 
-console.log(typeof imageNames);
-
 const Page = () => {
   return (
     <main>
